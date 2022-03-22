b634e9bd5cff980836accb17f685152f8df7b7d0
decorocco <decorocco9@hotmail.com>
2022-03-22 14:48:53 -0300

    grid feita e ui de começada

--- CBTC/Assets/_Scripts/GameManager.cs
+++ CBTC/Assets/_Scripts/GameManager.cs
@@ -2,16 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum GameState { START, GAME, ENDGAME }
+
+    public GameState gameState { get; private set; }
+
+    public delegate void ChangeStateDelegate();
+    public static ChangeStateDelegate changeStateDelegate;
+
+    private static GameManager _instance;
+
+    private GameManager()
     {
-        
+     
+        gameState = GameState.START;
+
+    }
+    public static GameManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new GameManager();
+        }
+
+        return _instance;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeState(GameState nextState)
+    {
+        if (nextState == GameState.GAME) Reset();
+
+        gameState = nextState;
+        changeStateDelegate();
+    }
+
+    private void Reset()
     {
         
     }
